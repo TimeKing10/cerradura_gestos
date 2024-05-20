dbf11e12930242cc0c07952926629bdeec6c75c5
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 17:53:57 -0500

    Update app.py

--- bombillos/app.py
+++ bombillos/app.py
@@ -1,16 +1,50 @@
-import paho.mqtt.client as paho
-import time
-import json
+import os
 import streamlit as st
-import cv2
-import numpy as np
-#from PIL import Image
-from PIL import Image as Image, ImageOps as ImagOps
-from keras.models import load_model
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+import time
+import paho.mqtt.client as paho
+
+st.title("Control por Voz de Cerradura Inteligente")
+
+st.write("Presiona el botón y habla para controlar la cerradura inteligente.")
+
+stt_button = Button(label="Iniciar reconocimiento de voz", width=200)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+broker = "broker.mqttdashboard.com"
+port = 1883
+client1 = paho.Client("Control_Cerradura")
 
 def on_publish(client, userdata, result):
-    print("el dato ha sido publicado \n")
-    pass
+    print("El dato ha sido publicado \n")
 
 def on_message(client, userdata, message):
     global message_received
@@ -18,44 +52,12 @@ def on_message(client, userdata, message):
     message_received = str(message.payload.decode("utf-8"))
     st.write(message_received)
 
-broker = "broker.mqttdashboard.com"
-port = 1883
-client1 = paho.Client("APP_CERR")
 client1.on_message = on_message
 client1.on_publish = on_publish
 client1.connect(broker, port)
 
-model = load_model('keras_model.h5')
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
-st.title("Cerradura Inteligente")
-
-img_file_buffer = st.camera_input("Toma una Foto")
-
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    # To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
-
-    newsize = (224, 224)
-    img = img.resize(newsize)
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Normalize the image
-    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # Run the inference
-    prediction = model.predict(data)
-    print(prediction)
-    if prediction[0][0] > 0.3:
-        st.header('Abriendo')
-        client1.publish("IMIA", "{'gesto': 'Abre'}", qos=0, retain=False)
-        time.sleep(0.2)
-    if prediction[0][1] > 0.3:
-        st.header('Cerrando')
-        client1.publish("IMIA", "{'gesto': 'Cierra'}", qos=0, retain=False)
-        time.sleep(0.2)
+if result:
+    if "GET_TEXT" in result:
+        voice_command = result.get("GET_TEXT")
+        st.write("Comando de voz reconocido:", voice_command)
+        client1.publish("cerradura_comandos", voice_command, qos=0, retain=False)
