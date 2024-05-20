fb1c24961047d0a3a6bbc8c1f5f8b30347213b4b
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 17:48:29 -0500

    Update app.py

--- bombillos/app.py
+++ bombillos/app.py
@@ -1,26 +1,61 @@
+import paho.mqtt.client as paho
+import time
+import json
 import streamlit as st
-import paho.mqtt.client as mqtt
+import cv2
+import numpy as np
+#from PIL import Image
+from PIL import Image as Image, ImageOps as ImagOps
+from keras.models import load_model
 
-# Configuración del cliente MQTT
-mqtt_broker = "broker.mqttdashboard.com"
-mqtt_port = 1883
-mqtt_topic = "bombillos/control"
-client = mqtt.Client("Bombillos_Client")
-client.connect(mqtt_broker, mqtt_port)
+def on_publish(client, userdata, result):
+    print("el dato ha sido publicado \n")
+    pass
 
-st.title("Control de los Bombillos con Palmadas")
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received = str(message.payload.decode("utf-8"))
+    st.write(message_received)
 
-# Función para publicar mensajes MQTT para encender o apagar los bombillos
-def publish_light_command(command):
-    client.publish(mqtt_topic, command)
+broker = "broker.mqttdashboard.com"
+port = 1883
+client1 = paho.Client("APP_CERR")
+client1.on_message = on_message
+client1.on_publish = on_publish
+client1.connect(broker, port)
 
-# Control de los bombillos con palmadas
-if st.button("Encender Bombillos con Palmadas"):
-    st.write("Escuchando palmadas...")
-    # Aquí puedes implementar la lógica para detectar palmadas y publicar el comando MQTT correspondiente
-    # Ejemplo:
-    # if detect_claps():
-    #     publish_light_command("encender")
+model = load_model('keras_model.h5')
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-if st.button("Apagar Bombillos"):
-    publish_light_command("apagar")
+st.title("Cerradura Inteligente")
+
+img_file_buffer = st.camera_input("Toma una Foto")
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    newsize = (224, 224)
+    img = img.resize(newsize)
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Normalize the image
+    normalized_image_array = (img_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Run the inference
+    prediction = model.predict(data)
+    print(prediction)
+    if prediction[0][0] > 0.3:
+        st.header('Abriendo')
+        client1.publish("IMIA", "{'gesto': 'Abre'}", qos=0, retain=False)
+        time.sleep(0.2)
+    if prediction[0][1] > 0.3:
+        st.header('Cerrando')
+        client1.publish("IMIA", "{'gesto': 'Cierra'}", qos=0, retain=False)
+        time.sleep(0.2)
