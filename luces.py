11d2a069535666a9823751317b0e11181eac2966
TimeKing10 <160857880+TimeKing10@users.noreply.github.com>
2024-05-20 22:04:18 -0500

    Update luces.py

--- luces.py
+++ luces.py
@@ -13,14 +13,16 @@ def send_message(topic, gesture):
     client1.connect(broker, port)
     client1.on_publish = on_publish
     message = json.dumps({"gesto": gesture})
-    ret = client1.publish(topic, message)
+    client1.publish(topic, message)
 
 st.title("Control de Luces")
 
 if st.button('Encender'):
-    client1.publish("IMIA","{'gesto': 'Prender'}",qos=0, retain=False)
+    client1.connect(broker, port)
+    send_message("IMIA", "Prender")
     st.success("El LED ha sido encendido.")
 
 if st.button('Apagar'):
-    client1.publish("IMIA","{'gesto': 'Apagar'}",qos=0, retain=False)
+    client1.connect(broker, port)
+    send_message("IMIA", "Apagar")
     st.success("El LED ha sido apagado.")
